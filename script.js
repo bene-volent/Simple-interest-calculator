1aa31bd7cd5126932ce943efe1229b5044179934
Bene-volent <raghavwastaken1@gmail.com>
2022-08-13 11:21:50 +0530

    Commit

---
+++ script.js
@@ -0,0 +1,29 @@
+function compute()
+{
+    /**
+     * * Computes the total amount of interest 
+     */
+    var principal = document.getElementById("principal").value;
+    if (parseFloat(principal)<=0 || principal==''){
+        alert("Enter a positive number")
+        document.getElementById('principal').focus()
+        return
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * rate * years / 100
+
+    var year = new Date().getFullYear() + parseInt(years);
+
+    var result = document.getElementById('result')
+
+    result.innerHTML = `If you deposit ${getHighlighter(principal)},\<br>at an interest rate of ${getHighlighter(rate.toString() + '%')}.\<br>You will recieve an amount of ${getHighlighter(parseFloat(interest))},\<br>in the year ${getHighlighter(year)}`
+}
+        
+function updateRate(){
+    document.getElementById('rate_val').innerHTML = `${document.getElementById("rate").value}%`
+
+}
+function getHighlighter(text){
+    return `<span class='highlighted'>${text}</span>`
+}
